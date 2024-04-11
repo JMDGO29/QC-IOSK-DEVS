cd26eb712217163f2adfe4d566812cf1f58927c2
Mike Gester <mikegester.sabuga023@gmail.com>
2024-04-11 09:07:08 +0800

    added various functions for navigations

--- src/components/campus/sanBartolome/animation/Animation.tsx
+++ src/components/campus/sanBartolome/animation/Animation.tsx
@@ -9,14 +9,15 @@ import { useHistory } from "react-router";
 import ModelViewer from "../ModelViewer";
 import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "../../../utils/firebase";
-
+import textGuide from '../../../../data/textGuide';
+import Modal from "react-modal";
 interface ContainerProps {
   name: string;
   roomName: string;
   modelPath: string;
   voice?: string;
   shortPath?: string;
-
+  textGuide?: string[];
   selectedBuilding: string;
   selectedFloor: string;
   selectedRoom: string;
@@ -42,47 +43,37 @@ interface otherModel {
 const AnimatedModelViewer = ({ modelPath, mixer }: any) => {
   const { scene, animations, cameras } = useGLTF(modelPath) as unknown as GLTF;
 
-  // useEffect(() => {
-  //   if (animations) {
-  //     const animationAction = mixer.clipAction(animations[0]); // Assuming there's only one animation
-  //     animationAction.setLoop(THREE.LoopOnce); // Set the loop to play only once
-  //     animationAction.clampWhenFinished = true; // Keep the last frame displayed after the animation finishes
-  //     animationAction.play();
-  //     console.log("Animation Name:", animations[0].name);
 
-  //     animationAction.onFinish = () => {
-  //       // Stop the animation when it finishes playing
-  //       animationAction.stop();
-  //     };
-  //   }
-  // }, [animations, mixer]);
 
   useEffect(() => {
-    if (animations) {
+    if (animations && mixer) {
       animations.forEach((clip: THREE.AnimationClip) => {
         const action = mixer.clipAction(clip);
-        action.play();
-        console.log("Animation Name:", clip.name);
+        action.reset(); // Reset the animation
+        action.play(); // Start the animation from the beginning
       });
     }
-  }, [animations, cameras, mixer]);
+  }, [animations, mixer]);
+
+
 
   useFrame((state, delta) => {
-    mixer.update(delta);
+    if (mixer) {
+      mixer.update(delta);
+    }
   });
-
   return (
     <>
       <primitive object={scene} position={[0, 0, 0]} />
     </>
   );
 };
-
 const Animation: React.FC<ContainerProps> = ({
   name,
   roomName,
   modelPath,
   voice,
+  textGuide,
   shortPath,
   selectedBuilding,
   selectedFloor,
@@ -93,13 +84,51 @@ const Animation: React.FC<ContainerProps> = ({
   const [activeCameraIndex, setActiveCameraIndex] = useState(0);
   const mixerRef = useRef<THREE.AnimationMixer | null>(null);
   const audioRef = useRef<HTMLAudioElement>(null);
-
+  const [qrModal, setQrModal] = useState(false);
   const [buildings, setBuildings] = useState<Building[]>([]);
   const [otherModel, setOtherModel] = useState<otherModel[]>([]);
+  const [resetScene, setResetScene] = useState(false);
+  const [initialCameraPosition, setInitialCameraPosition] = useState<
+    [number, number, number]
+  >([0, 0, 0]);
 
   const handleCameraSwitch = () => {
     setActiveCameraIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
   };
+
+  const handleResetScene = () => {
+    setResetScene((prevReset) => !prevReset); // Toggle the state to trigger a re-render
+    if (mixerRef.current && scene.animations && scene.animations.length > 0) {
+      mixerRef.current.setTime(0); // Set time to start of animation
+    }
+    if (cameras && cameras.length > 0 && initialCameraPosition) {
+      cameras[activeCameraIndex].position.set(...initialCameraPosition);
+    }
+  };
+
+  useEffect(() => {
+    if (cameras && cameras.length > 0) {
+      const initialPosition = [
+        cameras[activeCameraIndex].position.x,
+        cameras[activeCameraIndex].position.y,
+        cameras[activeCameraIndex].position.z,
+      ] as [number, number, number]; // Convert to tuple type
+      setInitialCameraPosition(initialPosition);
+    }
+  }, [cameras, activeCameraIndex]);
+
+  const clickHome = () => {
+    window.location.reload();
+  }
+
+
+  const clickQr = () => {
+    setQrModal(true);
+  }
+
+  const closeModal = () => {
+    setQrModal(false);
+  }
 
   useEffect(() => {
     let timeoutId: any;
@@ -150,7 +179,7 @@ const Animation: React.FC<ContainerProps> = ({
     };
 
     fetchBuildings();
-  }, []);
+  }, [resetScene]);
 
   useEffect(() => {
     const fetchModels = async () => {
@@ -169,30 +198,93 @@ const Animation: React.FC<ContainerProps> = ({
     };
 
     fetchModels();
-  }, []);
+  }, [resetScene]);
 
   return (
     <>
-      <div className="fixed z-20 w-full h-auto bg-red-500 bottom-2 ">
-        <div className="flex items-start justify-center space-x-3">
-          <div
-            tabIndex={0}
-            className="z-30 border-collapse collapse collapse-arrow bg-base-200 text-base-content w-96"
-          >
-            <input type="checkbox" />
-            <div className="flex items-center justify-between collapse-title">
-              <p className="text-xl font-bold">Room: {roomName}</p>
+      <div className="fixed bottom-0 right-0 z-50 w-auto h-auto">
+        <div className="flex items-start justify-start space-x-3">
+          <div className="h-screen w-[400px] bg-base-100">
+            <div className="relative flex flex-col justify-between h-full p-3 space-y-3 bg-transparent gap-">
+              <div className="flex flex-col w-full h-24 space-y-5 min-h-10">
+                <div className="h-auto p-6 bg-base-300 rounded-3xl ">
+                  <p className="text-xs font-bold text-base-content align-super">Room {selectedRoom}</p>
+                  <p className="text-3xl font-bold text-base-content">{roomName}</p>
+                </div>
+                <div className="w-full h-[700px] p-3 bg-base-300 rounded-3xl">
+                  <div className="h-auto max-h-full p-6 overflow-y-auto bg-base-300 rounded-3xl ">
+                    <p className="text-xl font-bold text-base-content">
+                      {Array.isArray(textGuide) && (
+                        <ul className="steps steps-vertical">
+                          {textGuide.map((guide, index) => (
+                            <li key={index} className=" step step-accent">
+                              <p className="text-left">{guide}</p>
+                            </li>
+                          ))}
+                          {/* <li className="step step-primary">Choose plan</li>
+                          <li className="step">Purchase</li>
+                          <li className="step">Receive Product</li>
+                          <li className="step">Receive Product asdasdas asdasd sad asd asd asd</li>
+                          <li className="step">Receive Product</li>
+                          <li className="step">Receive Product</li>
+                          <li className="step">Receive Product</li>
+                          <li className="step">Receive Product</li>
+                          <li className="step">Receive Product</li> */}
+
+
+                        </ul>
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full h-24 bg-base-300/80 text-base-content rounded-3xl">
+                <div className="flex items-center justify-around w-full h-24 px-3 ">
+                  <button
+                    onClick={clickHome}
+                    className="border-none shadow-none btn-square rounded-2xl btn btn-lg hover:btn-accent"
+                  >
+                    <Icon
+                      icon="octicon:home-16"
+                      className="w-10 h-10 text-base-content"
+                    />
+                  </button>
+                  <button
+                    onClick={handleResetScene}
+                    className="border-none shadow-none btn-square rounded-2xl btn btn-lg hover:btn-accent"
+                  >
+                    <Icon
+                      icon="mi:refresh"
+                      className="w-10 h-10 text-base-content"
+                    />
+                  </button>
+                  <button
+                    onClick={handleCameraSwitch}
+                    className="border-none shadow-none btn-square rounded-2xl btn btn-lg hover:btn-accent"
+                  >
+                    <Icon
+                      icon="icon-park-outline:flip-camera"
+                      className="w-10 h-10 text-base-content"
+                    />
+                  </button>
+                  <button
+                    onClick={clickQr}
+                    className="border-none shadow-none btn-square rounded-2xl btn btn-lg hover:btn-accent"
+                  >
+                    <Icon
+                      icon="mingcute:qrcode-2-line"
+                      className="w-10 h-10 text-base-content"
+                    />
+                  </button>
+
+
+                </div>
+              </div>
             </div>
+            <p className="text-xl font-bold">Room: {roomName}</p>
           </div>
-          <button
-            onClick={handleCameraSwitch}
-            className="btn-square rounded-2xl btn mt-1.5 btn-accent"
-          >
-            <Icon
-              icon="icon-park-outline:flip-camera"
-              className="w-10 h-10 text-base-content"
-            />
-          </button>
+
         </div>
       </div>
       <Canvas
@@ -210,6 +302,7 @@ const Animation: React.FC<ContainerProps> = ({
         <ambientLight intensity={2} />
         <Clouds />
         <AnimatedModelViewer
+          key={resetScene ? "reset" : "default"}
           modelPath={modelPath}
           mixer={mixerRef.current}
           activeCameraIndex={activeCameraIndex}
@@ -237,6 +330,26 @@ const Animation: React.FC<ContainerProps> = ({
         ))}
       </Canvas>
       <audio ref={audioRef} src={voice} />
+    
+        <Modal
+          className="flex items-center justify-center w-screen h-screen transition-all duration-150 ease-in-out bg-black/60"
+          isOpen={qrModal}
+          onRequestClose={() => setQrModal(false)}
+          contentLabel="Alert"
+        >
+          <div className="p-6 shadow-xl h-96 bg-base-100 rounded-2xl w-96">
+            <p className="text-3xl text-center"></p>
+            <div className="flex justify-center space-x-3 mt-14">
+              <button onClick={closeModal} className="btn bg-base-300">
+                <Icon icon="line-md:close-small" className="w-10 h-10" />
+                Close
+              </button>
+            </div>
+          </div>
+        </Modal>
+
+
+
     </>
   );
 };
